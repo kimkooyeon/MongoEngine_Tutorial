91461b233e9e477079f118d0adc3e909dd603bf7
kimkooyeon git <kimkooyeon@gmail.com>
2019-11-02 23:07:19 +0800

    Starting up

--- mongoengine_tutorial.py
+++ mongoengine_tutorial.py
@@ -2,3 +2,11 @@ from mongoengine import *
 
 result = connect('test', host='test', port=123)
 print(result)
+
+print("Testing!")
+
+
+class User(Document):
+    username = StringField(required=True)
+    password = StringField(required=True)
+    email = StringField(unique=True, required=True)
