abb1466158d2ca396b77cf139a45d8fd0c87a876
kimkooyeon git <kimkooyeon@gmail.com>
2019-11-03 22:17:54 +0800

    Starting up

---
+++ settingSecret.py
@@ -0,0 +1,6 @@
+class MyMongoDBServerTest01():
+    username = 'testuser'
+    password = 'RP7617D6djGCaOhe'
+    host = 'cluster0-265t9.mongodb.net/test?retryWrites=true&w=majority'
+    hostURI = 'mongodb+srv://testuser:RP7617D6djGCaOhe@cluster0-265t9.mongodb.net/test?retryWrites=true&w=majority'
+    full_connection_string = "mongodb+srv://testuser:"+password+"@cluster0-265t9.mongodb.net/test?retryWrites=true&w=majority"
